94044ac8df6d7b2f79178a7100163f7a3d2e91f4
Jabri <donjabricio@gmail.com>
2017-11-10 07:27:28 +0100

    Admin

--- server/boot/migration.js
+++ server/boot/migration.js
@@ -1,20 +1,52 @@
 'use strict';
 
-module.exports = function(app) {
+module.exports = function (app) {
     //data sources
     var mysqlDs = app.dataSources.mysqlDs;
-    if(process.env.AUTOMIGRATE) {
+    var Usuario = app.models.Usuario;
+    var Role = app.models.Role;
+    var RoleMapping = app.models.RoleMapping;
+    
+    if (process.env.AUTOMIGRATE) {
         mysqlDs.automigrate(null, function (err) {
-            if (err) console.log(err);
+            if (err)
+                console.log(err);
             console.log('> Models migrated to tables');
-            
-            app.loadFixtures()
-            .then(function() {
-              console.log('Done!');
-            })
-            .catch(function(err) {
-              console.log('Errors:', err);
+
+            Usuario.create(
+                    {nombre: 'Bob',
+                        apellidos: 'Viajes Educativos',
+                        email: process.env.ADMIN_EMAIL,
+                        password: process.env.ADMIN_PASSWORD}
+            , function (err, user) {
+                if (err)
+                    return console.log(err);
+
+                //create the admin role
+                Role.create({
+                    name: 'administrador'
+                }, function (err, role) {
+                    if (err)
+                       console.log(err);
+
+                    //make bob an admin
+                    role.principals.create({
+                        principalType: RoleMapping.USER,
+                        principalId: user.id
+                    }, function (err, principal) {
+                        console.log(err);
+                    });
+                });
             });
+
+
+            app.loadFixtures()
+                    .then(function () {
+                        console.log('Done!');
+                    })
+                    .catch(function (err) {
+                        console.log('Errors:', err);
+                    });
         });
     }
 }
