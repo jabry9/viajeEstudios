8f9dcffa8dff54ae306a3e640b29560d3fd87dd0
Jabri <donjabricio@gmail.com>
2017-11-13 07:23:27 +0100

    SendMail

--- common/models/centro.js
+++ common/models/centro.js
@@ -1,6 +1,31 @@
 'use strict';
 
-module.exports = function(Centro) {
-	Centro.validatesNumericalityOf('codigocentro', {int: true, message: 'Debe ser un número sin decimales'});
-	Centro.validatesUniquenessOf('codigocentro', {message: 'Ese centro ya existe'});
+module.exports = function (Centro) {
+    
+    Centro.validatesNumericalityOf('codigocentro', {int: true, message: 'Debe ser un número sin decimales'});
+    Centro.validatesUniquenessOf('codigocentro', {message: 'Ese centro ya existe'});
+    
+    Centro.beforeRemote('create',function (context, centro, next) {
+        centro.verificado = 0;
+        context.verificado = 0;
+        next();
+    });
+    
+    Centro.afterRemote('create', function (context, centro, next) {
+        
+        var html = 'Nuevo registro de centro '+centro.nombre+"  con id "+centro.codigocentro+" en la localidad de "+centro.localidad ;
+        centro.verificado = false;
+        Centro.app.models.Email.send({
+            to: process.env.ADMIN_EMAIL,
+            from: process.env.EMAIL_USER,
+            subject: 'Nuevo centro',
+            html: html
+        }, function (err) {
+            if (err)
+                return console.log('> error sending email');
+            console.log('> sending email ok');
+        });
+        next();
+
+    });
 };
