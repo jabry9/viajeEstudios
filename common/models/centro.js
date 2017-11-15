46cb891de80c9693599c35a82492ed83ec2199f6
Jabri <donjabricio@gmail.com>
2017-11-15 11:12:02 +0100

    mio

--- common/models/centro.js
+++ common/models/centro.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function(Centro) {
+module.exports = function (Centro) {
     Centro.validatesNumericalityOf('codigocentro', {int: true, message: 'Debe ser un número sin decimales'});
     Centro.validatesUniquenessOf('codigocentro', {message: 'Ese centro ya existe'});
 
@@ -9,7 +9,34 @@ module.exports = function(Centro) {
         context.args.data.coordinador = context.req.accessToken.userId;
         next();
     });
-  
+
+    Centro.beforeRemote('find', function (context, centro, next) {
+        var rolem = Centro.app.models.RoleMapping;
+        if (context.req.accessToken == null) {
+            context.args.filter = {where: {"verificado": true}};
+            console.log("Sin user");
+        } else {
+
+            var id = context.req.accessToken.userId;
+            console.log(id);
+            var t = rolem.findOne({where: {principalId: id}, include: {relation: "role", scope: {where: {name: "administrador"}}}}, function(err, role) { console.log(role) });
+            console.log(t);
+            if (null !== null) {
+                console.log("Es admin " + t);
+            } else {
+                console.log("No lo es");
+            }
+
+        }
+
+
+
+
+
+        next();
+
+    });
+
     Centro.afterRemote('create', function (context, centro, next) {
         var options = {
             type: 'email',
@@ -18,7 +45,6 @@ module.exports = function(Centro) {
             subject: 'Alta de centro pendiente de validación',
             html: "Centro pendiente de validación"
         };
-
         Centro.app.models.Email.send(options, function (err) {
             if (err)
                 return console.log('> error sending verificación de centro al admin');
@@ -26,5 +52,5 @@ module.exports = function(Centro) {
         });
         next();
     });
-
-};
+}
+;
